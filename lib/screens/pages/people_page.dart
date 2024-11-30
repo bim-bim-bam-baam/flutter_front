48076920f7e7a6b3ac69f1bc4754538a0086c0f1
sour-soup <kiziyepi8@gmail.com>
2024-11-30 17:49:03 +0400

    fix people page

--- lib/screens/pages/people_page.dart
+++ lib/screens/pages/people_page.dart
@@ -253,6 +253,7 @@ void _showSuccessDialog(String message) {
                     child: ListTile(
                       leading: CircleAvatar(
                         backgroundColor: const Color(0xFFBB86FC),
+                        backgroundImage: NetworkImage(person['avatar']),
                         child: Text(
                           person['username'],
                           style: const TextStyle(
